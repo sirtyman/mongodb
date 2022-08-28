86b6838a0bda3fba140b7417f5c82c1553e396e9
Marcin <marcin.tyman@gmail.com>
2022-08-28 19:21:36 +0200

    Add reading credentials from file.

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # mongodb
 A quick tutorial of MongoDB [Python]
+
+To start with create json credentials.json with the following content:
+
+{
+    "username": "<username>",
+    "password": "<passwd>"
+}
--- main.py
+++ main.py
@@ -1,12 +1,26 @@
 import pprint
+
 from pymongo import MongoClient
+import json
 
 
-connection = "mongodb+srv://sirtyman:<passwd>@cluster0.mb0zwid.mongodb.net/?retryWrites=true&w=majority"
+with open("./credentials.json") as f:
+    json_content = f.read()
+    
+cred = json.loads(json_content)
+
+
+
+connection = f'mongodb+srv://{cred["username"]}:{cred["password"]}@cluster0.mb0zwid.mongodb.net/?retryWrites=true&w=majority'
+
 client = MongoClient(connection)
 
+
 db = client.sample_restaurants
+
 collection = db.restaurants
 
-for doc in collection.find({'name': 'La Bella Pizza'}).batch_size(1000):
+
+for doc in collection.find({'name': 'La Bella Pizza'}):
     pprint.pprint(doc)
+
